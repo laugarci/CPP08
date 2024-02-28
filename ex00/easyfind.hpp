7fff915a8540282a0fcffb98c19da106f346926a
Laura Garcia Arch <garch.laura@gmail.com>
2024-02-28 12:38:34 +0100

    cpp08

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,5 +1,12 @@
 
 #pragma once
+
+#define BOLD "\x1B[1m"
+#define RESET "\x1B[0m"
+#define RED "\x1B[31m"
+#define BGRED "\x1B[41m"
+#define GREEN "\x1B[32m"
+
 #include <vector>
 #include <iostream>
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,15 +5,61 @@ int main()
 {
 	std::vector<int> vec;
 	vec.push_back(0);
+	vec.push_back(11);
+	vec.push_back(24);
+	vec.push_back(36);
+	vec.push_back(41);
+	vec.push_back(50);
+	vec.push_back(90);
+	vec.push_back(5);
 	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(2);
-	easyfind(vec, 2);
 
+	std::cout << BGRED "Correct test" << RESET << std::endl;
 	try {
-		int index = easyfind(vec, 2);
-		std::cout << "Value found at index: " << index << std::endl; 
+		int index;
+		int val;
+		
+		val = 5;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 24;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 1;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 41;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 90;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 0;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+		val = 1;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+		std::cout << BGRED "Incorrect test" << RESET << std::endl;
+	try {
+		int index;
+		int val;
+		val = 2;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+		try {
+		int index;
+		int val;
+
+		val = 100;
+		index = easyfind(vec, val);
+		std::cout << "Value to search: " << val << std::endl << "Value found at index: " << index << std::endl;
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
